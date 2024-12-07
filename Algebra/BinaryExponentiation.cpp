1eb9fbcbab7ef242cf895a07598983b8b77689b7
Bikash Chauhan <chauhanbikash789@gmail.com>
2024-12-07 21:33:02 +0545

    Add binary-exponentiation

--- Algebra/BinaryExponentiation.cpp
+++ Algebra/BinaryExponentiation.cpp
@@ -1,9 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+long long power(long long base, long long exp)
+{
+    // base case
+    if(exp == 0) return 1;
+
+    // recursive call
+    long long ans = power(base, exp / 2);
+    if(exp & 1)
+    {
+        ans *= ans * base;
+    }
+    else ans *= ans;
+    return ans;
+}
 
 int main()
 {
-    
+    cout<<power(4, 2)<<endl;
     return 0;
 }
